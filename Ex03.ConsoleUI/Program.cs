ec9b276ca0efa0cafe0a9efb597410e50a184d87
snirkol <snirkol@gmail.com>
2018-05-05 14:04:27 +0300

    Electrice car changes and View changes

--- Ex03.ConsoleUI/Program.cs
+++ Ex03.ConsoleUI/Program.cs
@@ -15,7 +15,13 @@ namespace Ex03.ConsoleUI
             Console.WriteLine("Welcome to the garage");
             while (true)
             {
-                Console.WriteLine("Eneter licenseNumber of car to create one!");
+                Console.WriteLine("1. Enter Vehicle to the garage");
+                Console.WriteLine("2. Get List of all vehicle by type");
+                Console.WriteLine("3. Chenge exist vehicle status");
+                Console.WriteLine("4. Refule vehicle");
+                Console.WriteLine("5. Charge electrice vehicle");
+                Console.WriteLine("6. Vehicle detail");
+
                 string lisenceNumber = Console.ReadLine();
                 try
                 {
--- Ex03.GarageLogic/ElectricMotorcar.cs
+++ Ex03.GarageLogic/ElectricMotorcar.cs
@@ -12,21 +12,25 @@ namespace Ex03.GarageLogic
         float m_MaxBatteryTime;
         Motorcar m_Motorcar;
 
-        public ElectricMotorcar(string i_LicenseNumber, float i_BatteryTime, float i_MaxBatteryTime, eColors i_Color, eNumberOfDoors i_NumOfDoors) : base(i_LicenseNumber)
+        public ElectricMotorcar(string i_LicenseNumber, float i_BatteryTime, float i_MaxBatteryTime,
+            eColors i_Color, eNumberOfDoors i_NumOfDoors) : base(i_LicenseNumber)
         {
             m_BatteryTime = i_BatteryTime;
             m_MaxBatteryTime = i_MaxBatteryTime;
             m_Motorcar = new Motorcar (i_Color, i_NumOfDoors);
         }
 
-        public void ChargingBattery(float i_TimeToCharge)
+        public void ChargeBattery(float i_TimeToCharge)
         {
             if (m_BatteryTime + i_TimeToCharge <= m_MaxBatteryTime)
             {
                 m_BatteryTime += i_TimeToCharge;
             }
+            else
+            {
+                throw new ValueOutOfRangeException(0, m_MaxBatteryTime);
+            }
 
-            //TODO: if is grater from max time -> throw exception
         }
     }
 }
