14626822d1c6f953f81ee331c95d8fe6cf976134
snirkol <snirkol@gmail.com>
2018-05-06 00:23:26 +0300

    vehicle generator

--- Ex03.GarageLogic/vehicleGenerator.cs
+++ Ex03.GarageLogic/vehicleGenerator.cs
@@ -8,10 +8,20 @@ namespace Ex03.GarageLogic
 {
     static class VehicleGenerator
     {
-        public static Vehicle CreateNewVehicle(string i_LicenseNumber)
+        public static Vehicle CreateNewVehicle(string i_Type, string i_LicenseNumber, string i_ModelName,float i_PercentageOfEnergyRemaining,
+            float i_TiresPressure, string i_OwnerName , string i_OwnerTelephone, string i_OtherDetails)
         {
-            Vehicle newVehicle = new Vehicle(i_LicenseNumber);
-            return newVehicle; 
+            Vehicle vehicle = null;
+
+            switch (i_Type)
+            {
+                case "truck":
+                    break;
+            }
+            //Vehicle newVehicle = new Vehicle(i_LicenseNumber);
+            //return newVehicle; 
+
+            return vehicle;
         }
     }
 }
