94b2edbc119407dadfa82b9b4c177ecbfbb1687d
chen lahav <chenlahav6@gmail.com>
2018-05-05 15:10:50 +0300

    added lorry class

---
+++ Ex03.GarageLogic/Lorry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    class Lorry : Vehicle
+    {
+        bool m_IsTrunkCooled;
+        float m_TrunckCapacity;
+
+        public Lorry(string i_LicencseNumber, bool i_IsTrunkCooled, float i_TrunckCapacity) : base(i_LicencseNumber)
+        {
+            m_IsTrunkCooled = i_IsTrunkCooled;
+            m_TrunckCapacity = i_TrunckCapacity;
+        }
+    }
+}
