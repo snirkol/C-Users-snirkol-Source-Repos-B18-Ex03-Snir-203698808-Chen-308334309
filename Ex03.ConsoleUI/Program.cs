418f1339a3925536166449ccb13e8de381a22d5f
chen lahav <chenlahav6@gmail.com>
2018-05-05 18:44:45 +0300

    added logic in UI

--- Ex03.ConsoleUI/Program.cs
+++ Ex03.ConsoleUI/Program.cs
@@ -12,28 +12,37 @@ namespace Ex03.ConsoleUI
         public static void Main()
         {
             Garage garage = new Garage();
-            Console.WriteLine("Welcome to the garage");
+            Console.WriteLine("Welcome to the garage\nSelect your option:");
             while (true)
             {
                 Console.WriteLine("1. Enter Vehicle to the garage");
                 Console.WriteLine("2. Get List of all vehicle by type");
-                Console.WriteLine("3. Chenge exist vehicle status");
+                Console.WriteLine("3. Change exist vehicle status");
                 Console.WriteLine("4. Refule vehicle");
                 Console.WriteLine("5. Charge electrice vehicle");
                 Console.WriteLine("6. Vehicle detail");
 
-                string lisenceNumber = Console.ReadLine();
-                try
+                int selectedOption = int.Parse(Console.ReadLine());
+                switch(selectedOption)
                 {
-                    garage.AddNewVehicle(lisenceNumber);
-                }
-                catch (ArgumentException ae)
-                {
-                    Console.WriteLine("Vehicle already exist in the Garage!");
-                }
-                catch(Exception e)
-                {
-                    Console.WriteLine("error!");
+                    case 1:
+                        {
+                            Console.WriteLine("Enter lisenceNumber");
+                            string lisenceNumber = Console.ReadLine();
+                            try
+                            {
+                                garage.AddNewVehicle(lisenceNumber);
+                            }
+                            catch (ArgumentException ae)
+                            {
+                                Console.WriteLine("Vehicle already exist in the Garage!");
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("error!");
+                            }
+                            break;
+                        }
                 }
             }
         }
