a4e698d0a0715af891943ae2c7577dc7a4447a46
snirkol <snirkol@gmail.com>
2018-05-05 12:35:19 +0300

    ValueOutOfRangeException

---
+++ Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    class ValueOutOfRangeException : Exception
+    {
+        float m_minValue;
+        float m_maxValue;
+
+        public ValueOutOfRangeException(float i_MinValue, float i_MaxValue)
+        {
+            m_minValue = i_MinValue;
+            m_maxValue = i_MaxValue;
+        }
+    }
+}
